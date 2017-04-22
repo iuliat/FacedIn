67bc3b2c42f05b62cd4d686f6843b59029626665
p44v9n <paavan.buddhdev@gmail.com>
2017-04-22 17:05:12 +0200

    taylorcoffelts edge detection

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
+
+
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+
 
 function draw() {
 	// TODO check if valid URL before doing this function 
 
 	var img = new Image;
 	img.onload = draw;
-	img.src = document.getElementById('image-url').value;
-	img.scale
+	var url = document.getElementById('image-url').value;
+	img.src = url + '?' + new Date().getTime();
+	img.setAttribute('crossOrigin', '');
+
+
 
 	// TODO resize image
 
 	var dArr = [-1,-1, 0,-1, 1,-1, -1,0, 1,0, -1,1, 0,1, 1,1]; // offset array
-	var s = 1	;  // thickness scale
-	var i = 0;  // iterator
-	var x = 5;  // final position
+	// thickness scale
+	var s = 1;
+  	// final position
+	var x = 5;
 	var y = 5;
 
 	// draw images at offsets from the array scaled by s
-	for(; i < dArr.length; i += 2) {
+	for(var i = 0; i < dArr.length; i += 2) {
 		ctx.drawImage(img, x + dArr[i]*s, y + dArr[i+1]*s);
 	}
 
@@ -30,13 +37,17 @@ function draw() {
 	// draw original image in normal mode
 	ctx.globalCompositeOperation = "source-over";
 	ctx.drawImage(img, x, y);
+
+	console.log("image data: " + ctx.getImageData(10,10,50,50));
 }
+
+
 
 
 function outline() {
 	var root = (375,375);
-	var path1 = new Path2D();
-	path1.rect(10, 10, 100,100);
-
+    ctx.beginPath();
+	ctx.lineT
+o
 
 }
--- js/test.js
+++ js/test.js
@@ -2,15 +2,16 @@ function draw() {
   var canvas = document.getElementById('canvas');
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');    
-    var rootX = 375;
-    var rootY = 375;
+    var rootX = 150;
+    var rootY = 300;
     
-    var points = [(0, 200), (25, 175), (50, 150), (75, 125), (100, 100), (125, 75), (150, 50), (175, 25), (200, 0)]
+    var points = [[-25, 225], [0, 200], [25, 175], [50, 150], [75, 125], [100, 100], [125, 75], [150, 50], [175, 25], [200, 0], [225, -25]]
     
     ctx.beginPath();
     for (var i = 0; i < points.length; i++) {
-      var point = points[i];
-    	ctx.moveTo(point);
+      var pointX = points[i][0];
+      var pointY = points[i][1];
+    	ctx.moveTo(pointX, pointY);
       ctx.lineTo(rootX, rootY);
     }
     ctx.stroke();
