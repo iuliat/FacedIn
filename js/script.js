6276b2698d0f264403573a94c2add0b5c47facc4
p44v9n <paavan.buddhdev@gmail.com>
2017-04-22 14:42:25 +0200

    index barebones

--- js/script.js
+++ js/script.js
@@ -1,67 +1,13 @@
-var el = document.getElementById('container');
-var files = [];
-var img = new Image;
-var URL_ = window.URL || window.webkitURL;
+function allowDrop(ev) {
+    ev.preventDefault();
+}
 
-img.addEventListener('load', function (e) {
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
 
-// Free link to the file
-URL_.revokeObjectURL(img.src);
-
-img.onload() = draw();
-// Draw image
-
-// // Process next
-// generatePreview();
-// }, false);
-
-// el.addEventListener('dragenter', function (e) {
-//   e.preventDefault();
-// }, false);
-
-// el.addEventListener('dragover', function (e) {
-//   e.preventDefault();
-// }, false);
-
-// el.addEventListener('drop', function (e) {
-//   e.preventDefault();
-//   files = Array.prototype.slice.call(e.dataTransfer.files, 0);
-
-//   generatePreview();
-// }, false);
-
-// function generatePreview() {
-//   if (files.length == 0) return;
-//   image.src = URL_.createObjectURL(files.shift());
-// }
-
-
-
-function draw() {
-
-	var ctx = document.createElement('canvas');
-	ctx.width = 100;
-	ctx.height = 100;
-	// ctx.getContext('2d').drawImage(img, 0, 0, 100, 100);
-
-	el.appendChild(ctx);
-	var dArr = [-1,-1, 0,-1, 1,-1, -1,0, 1,0, -1,1, 0,1, 1,1]; // offset array
-	var s = 2;  // thickness scale
-	var i = 0;  // iterator
-	var x = 5;  // final position
-	var y = 5;
-
-	// draw images at offsets from the array scaled by s
-	for(; i < dArr.length; i += 2) {
-		ctx.drawImage(img, x + dArr[i]*s, y + dArr[i+1]*s);
-	}
-
-	// fill with color
-	ctx.globalCompositeOperation = "source-in";
-	ctx.fillStyle = "red";
-	ctx.fillRect(0,0,canvas.width, canvas.height);
-
-	// draw original image in normal mode
-	ctx.globalCompositeOperation = "source-over";
-	ctx.drawImage(img, x, y);
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
 }
